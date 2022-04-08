b7550dbe587564140dff97fbf48df640c7e39b45
Lucas <Lucas@gmail.com>
2022-04-08 17:34:21 -0300

    prompt e condições ok2

--- java/java.js
+++ java/java.js
@@ -1,9 +1,11 @@
+// Variavéis
 let quantidadeCartas;
 let condicaoMaior;
 let condicaoMenor;
 let condicaoPar;
 let condicaoOk;
 
+//funções
 function checar() {
     quantidadeCartas  = prompt ("Com quantas cartas você quer jogar?");
     condicaoMaior = quantidadeCartas >= 4;
@@ -11,6 +13,11 @@ function checar() {
     condicaoPar = (quantidadeCartas % 2) === 0;
     condicaoOk = (condicaoMaior && condicaoMenor && condicaoPar)
 }
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+//Js da página
 checar();
 while (condicaoOk !== true) {   
     alert ("Número inválido, escolha um número par entre 4 e 14");
