41df66594a20c842f8602180ab30ef21a0b333f3
Lucas <Lucas@gmail.com>
2022-04-09 03:32:34 -0300

    falta clique na carta e bônus

--- java/java.js
+++ java/java.js
@@ -4,8 +4,11 @@ let condicaoMaior;
 let condicaoMenor;
 let condicaoPar;
 let condicaoOk;
+let cartas = ["bobrossparrot.gif", "bobrossparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "metalparrot.gif", "revertitparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "unicornparrot.gif", "unicornparrot.gif"];
+let cartasEmJogo = []
+const listaCartas = document.querySelector(".cartas")
 
-//funções
+//Declaração de funções
 function checar() {
     quantidadeCartas  = prompt ("Com quantas cartas você quer jogar?");
     condicaoMaior = quantidadeCartas >= 4;
@@ -17,9 +20,28 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
-//Js da página
+//Recebendo a quantidade de cartas
 checar();
 while (condicaoOk !== true) {   
     alert ("Número inválido, escolha um número par entre 4 e 14");
     checar();
+}
+
+//Armar o jogo
+for(let i = 0; i < quantidadeCartas; i ++) {
+    cartasEmJogo.push(cartas[i])
+}
+
+cartasEmJogo.sort(comparador)
+
+for(let i = 0; i < quantidadeCartas; i ++) {
+    listaCartas.innerHTML += `
+    <div class="carta">
+        <div class="frente estiloCarta">
+            <img src="/imagens/front.png" />
+        </div>
+        <div class="verso estiloCarta">
+            <img src="/imagens/${cartasEmJogo[i]}" />
+        </div>
+    </div>`
 }
