9e51c9d94902cdeb7b82171578ee04e11b0cf3be
Lucas <Lucas@gmail.com>
2022-04-11 03:57:58 -0300

    falta desvirar erro e bônus

--- java/java.js
+++ java/java.js
@@ -6,9 +6,11 @@ let condicaoPar;
 let condicaoOk;
 let virada1 = false;
 let virada2 = false;
-let cartasViradas;
+let cartasViradas = 0;
+let jogarNovamente;
+let totalJogadas = 0;
 
-//arrays
+//Arrays
 let cartas = ["bobrossparrot.gif", "bobrossparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "metalparrot.gif", "revertitparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "unicornparrot.gif", "unicornparrot.gif"];
 let cartasEmJogo = []
 const listaCartas = document.querySelector(".cartas")
@@ -25,33 +27,19 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 function virarCarta(elemento) {
-    if (virada1 === false) {
-        virada1 = elemento.querySelector(".verso").querySelector("img")
-        elemento.querySelector(".estiloCarta").classList.add("frente")
-        elemento.querySelector(".verso").classList.add("vira")
-        elemento.querySelector(".verso").classList.remove("verso")
-        console.log(virada1)
-    }
-    else {
-        virada2 = elemento.querySelector(".verso").querySelector("img")
-        elemento.querySelector(".estiloCarta").classList.add("frente")
-        elemento.querySelector(".verso").classList.add("vira")
-        elemento.querySelector(".verso").classList.remove("verso")
-        console.log(virada2)
-        if (virada1 == virada2) {
-           cartasViradas += 2;
-           console.log(cartasViradas)
-           virada1 = false
-           virada2 = false
-        }
-        else {
-        }
-    }
+    elemento.querySelector(".estiloCarta").classList.add("frente")
+    elemento.querySelector(".verso").classList.add("vira")
+    elemento.querySelector(".verso").classList.remove("verso")
 }
-function desvirarCarta() {
-    document.querySelector(".vira").classList.add("verso")
-    document.querySelector(".vira").classList.remove("vira")
-    document.querySelector(".frente").classList.remove("frente")
+function desvirarCarta1() {
+    document.querySelector(virada1[1]).querySelector(".vira").classList.add("verso")
+    document.querySelector(virada1[1]).querySelector(".vira").classList.remove("vira")
+    document.querySelector(virada1[1]).querySelector(".frente").classList.remove("frente")
+}
+function desvirarCarta2() {
+    document.querySelector(virada2[1]).querySelector(".vira").classList.add("verso")
+    document.querySelector(virada2[1]).querySelector(".vira").classList.remove("vira")
+    document.querySelector(virada2[1]).querySelector(".frente").classList.remove("frente")
 }
 
 //Recebendo a quantidade de cartas
@@ -70,7 +58,7 @@ cartasEmJogo.sort(comparador)
 
 for(let i = 0; i < quantidadeCartas; i ++) {
     listaCartas.innerHTML += `
-    <div class="carta" onclick="virarCarta(this)">
+    <div class="carta ${cartasEmJogo[i].slice(0, 3)}" onclick="jogada(this)">
         <div class="estiloCarta">
             <img src="/imagens/front.png" />
         </div>
@@ -78,4 +66,42 @@ for(let i = 0; i < quantidadeCartas; i ++) {
             <img src="/imagens/${cartasEmJogo[i]}" />
         </div>
     </div>`
+}
+
+//Jogando
+function jogada(elemento) {
+    if (virada1 === false) {
+        virada1 = elemento.classList
+        totalJogadas ++
+        virarCarta(elemento)
+        console.log(elemento)
+    }
+    else {
+        virada2 = elemento.classList
+        totalJogadas ++
+        virarCarta(elemento)
+        console.log(elemento)
+        if (virada1[1] == virada2[1]) {
+           cartasViradas += 2;
+           console.log(cartasViradas)
+           virada1 = false
+           virada2 = false
+           if (cartasViradas == quantidadeCartas) {
+               alert(`Parabéns! Você ganho em ${totalJogadas} jogadas!`)
+               jogarNovamente = prompt("Gostaria de jogar novamente?")
+               while (jogarNovamente != "sim" && jogarNovamente != "não") {
+                alert("Resposta inválida. Favor digitar 'sim' ou 'não'.")
+                jogarNovamente = prompt("Gostaria de jogar novamente?")
+               }
+               if (jogarNovamente == "sim") {
+                   checar()
+               } else {
+                   alert ("Obrigado por jogar! Esperamos te ver novamente em breve!")
+               }
+           }
+        }
+        else {
+            setTimeout(desvirarCarta1(), 1000)
+        }
+    }
 }
